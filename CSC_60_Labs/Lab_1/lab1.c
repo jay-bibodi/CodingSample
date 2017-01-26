c21b3237d74222e0d3479e34fd6ed60e44711cab
Bibodi Jay <jay23193@gmail.com>
2017-01-26 10:30:03 -0800

    Create lab1.c

---
+++ CSC_60_Labs/Lab_1/lab1.c
@@ -0,0 +1,16 @@
+/*------------*/
+/* Bibodi Jay Nikhil */
+/* Section 3 */
+/* Lab 1 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("\nLab 1 \n\n");
+	printf("Hi, Bibodi Jay Nikhil\n\n");
+	printf("The best preparation for tomorrow is doing your best today. \n\n");
+	return EXIT_SUCCESS;
+}
+/*------------*/
